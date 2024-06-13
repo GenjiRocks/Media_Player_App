f45e346b40f108debe40972ea480ac7017644f17
Ashwin <ashwindeepak1997@gmail.com>
2024-06-13 12:27:03 +0530

    June13

--- src/Components/Add.jsx
+++ src/Components/Add.jsx
@@ -18,12 +18,79 @@ function Add() {
     url:''
   });
 
-  const validateLink =(e) =>{
-    
+  // const validateLink =(e) =>{
+  //   const link = e.target.value;
+  //   if(link.endsWith('?feature=share')){
+  //       const yTkey = link.slice(-26,-15)
+  //       let embedLink = `https://www.youtube.com/embed/${yTkey}`
+  //       setVideo({...video,url:embedLink})
+  //       console.log(`if ${yTkey}`);
+  //   }
+  //   else if(link.startsWith('https://youtu.be')){
+  //     const yTkey = link.slice(17,28)
+  //     let embedLink = `https://www.youtube.com/embed/${yTkey}`
+  //     setVideo({...video,url:embedLink})
+  //     console.log(`else if ${yTkey}`);
+  //   }
+  //   else{
+  //     const yTkey = link.slice(-11)
+  //     let embedLink = `https://www.youtube.com/embed/${yTkey}`
+  //     setVideo({...video,url:embedLink})
+  //     console.log(`else ${yTkey}`);
+
+  //   }
+
+  // https://www.youtube.com/watch?v=EJ-fmIKVU5k&ab_channel=Psivewri
+  // https://youtu.be/EJ-fmIKVU5k?si=2p-zrh4iMWodZnuP
+  // <iframe width="560" height="315" src="https://www.youtube.com/embed/EJ-fmIKVU5k" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+  // }
+
+  // for clearing when click on cancel or close]
+  const handleClose1 = ()=>{
+    setVideo({
+      caption:"",
+      Image:"",
+      url:""
+      })
+  }
+
+  const handleUpload =(e)=>{
+    e.preventDefault();
+    const{caption,Image,url} = video
+    if(!caption || !Image || !url){
+      alert('Please fill form completely')
+    }
+    else{
+      if(url.endsWith('?feature=share')){
+              const yTkey = url.slice(-26,-15)
+              let embedLink = `https://www.youtube.com/embed/${yTkey}`
+              setVideo({...video,url:embedLink})
+              console.log(`if ${yTkey}`);
+          }
+          else if(url.startsWith('https://youtu.be')){
+            const yTkey = url.slice(17,28)
+            let embedLink = `https://www.youtube.com/embed/${yTkey}`
+            setVideo({...video,url:embedLink})
+            console.log(`else if ${yTkey}`);
+          }
+          else if(url.startsWith('https://www.youtube.com/watch?')){
+            const yTkey = url.slice(32,43)
+            let embedLink = `https://www.youtube.com/embed/${yTkey}`
+            setVideo({...video,url:embedLink})
+            console.log(`else if ${yTkey}`);
+          }
+          else{
+            const yTkey = url.slice(-11)
+            let embedLink = `https://www.youtube.com/embed/${yTkey}`
+            setVideo({...video,url:embedLink})
+            console.log(`else ${yTkey}`);
+      
+          }
+    }
   }
 
     const [show, setShow] = useState(false);
-
     const handleClose = () => setShow(false);
     const handleShow = () => setShow(true);
     console.log(video);
@@ -42,16 +109,16 @@ function Add() {
         <Modal.Body>
           <p>Please fill the following details</p>
           <form className='border p-3 rounded border-secondary'>
-            <input type="text" placeholder='Video Caption' className='form-control' onChange={(e)=>setVideo({...video,caption:e.target.value})}/>
-            <input type="text" placeholder='Video Image' className='form-control mt-3' onChange={(e)=>setVideo({...video,Image:e.target.value})}/>
-            <input type="text" placeholder='Video URL' className='form-control mt-3' onChange={(e)=>validateLink(e)}/>
+            <input type="text" placeholder='Video Caption' className='form-control' value={video.caption} onChange={(e)=>setVideo({...video,caption:e.target.value})}/>
+            <input type="text" placeholder='Video Image' className='form-control mt-3' value={video.Image} onChange={(e)=>setVideo({...video,Image:e.target.value})}/>
+            <input type="text" placeholder='Video URL' className='form-control mt-3' value={video.url} onChange={(e)=>setVideo({...video,url:e.target.value})}/>
           </form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={handleClose}>
+          <Button variant="secondary" onClick={handleClose1}>
             Cancel
           </Button>
-          <Button variant="warning" onClick={handleClose}>
+          <Button variant="warning" onClick={handleUpload}>
             Upload
           </Button>
         </Modal.Footer>
